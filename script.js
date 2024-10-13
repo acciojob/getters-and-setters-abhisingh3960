2887e9a3209600543b451e5b5c45bc1516148ba7
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-10-13 19:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //complete this code
-class Person {}
-
+class Person {
+	constructor(name , age){
+		this._name= name;
+		this._age= age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(value){
+		return this._age=value;
+	}
+}
+let Person = new Person();
 class Student extends Person {}
 
 class Teacher extends Person {}
